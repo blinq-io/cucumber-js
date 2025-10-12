3933d10f153e1e4ad8c66a8f3c24f5aaaf77c50d
liad123121 <liad.kimron@blinq.io>
2025-10-12 15:44:47 +0300

    added hooks support

--- src/formatter/helpers/report_generator.ts
+++ src/formatter/helpers/report_generator.ts
@@ -6,20 +6,27 @@ import { writeFileSync } from 'fs-extra'
 // type JsonException = messages.Exception
 import objectPath from 'object-path'
 type JsonTimestamp = number //messages.Timestamp
-type JsonStepType = 'Unknown' | 'Context' | 'Action' | 'Outcome' | 'Conjunction'
+type JsonStepType =
+  | 'Unknown'
+  | 'Context'
+  | 'Action'
+  | 'Outcome'
+  | 'Conjunction'
+  | 'After'
+  | 'Before'
 
 const URL =
   process.env.NODE_ENV_BLINQ === 'dev'
     ? 'https://dev.api.blinq.io/api/runs'
     : process.env.NODE_ENV_BLINQ === 'local'
-      ? 'http://localhost:5001/api/runs'
-      : process.env.NODE_ENV_BLINQ === 'stage'
-        ? 'https://stage.api.blinq.io/api/runs'
-        : process.env.NODE_ENV_BLINQ === 'prod'
-          ? 'https://api.blinq.io/api/runs'
-          : !process.env.NODE_ENV_BLINQ
-            ? 'https://api.blinq.io/api/runs'
-            : `${process.env.NODE_ENV_BLINQ}/api/runs`
+    ? 'http://localhost:5001/api/runs'
+    : process.env.NODE_ENV_BLINQ === 'stage'
+    ? 'https://stage.api.blinq.io/api/runs'
+    : process.env.NODE_ENV_BLINQ === 'prod'
+    ? 'https://api.blinq.io/api/runs'
+    : !process.env.NODE_ENV_BLINQ
+    ? 'https://api.blinq.io/api/runs'
+    : `${process.env.NODE_ENV_BLINQ}/api/runs`
 
 const REPORT_SERVICE_URL = process.env.REPORT_SERVICE_URL ?? URL
 const BATCH_SIZE = 10
@@ -555,6 +562,36 @@ export default class ReportGenerator {
     const testStep = this.testStepMap.get(testStepId)
     if (testStep.pickleStepId === undefined) {
       if (testStepResult.status === 'FAILED') {
+        const testCase = this.testCaseReportMap.get(
+          testStepFinished.testCaseStartedId
+        )
+        const type =
+          testCase.steps[0].result.status === 'UNKNOWN' ? 'Before' : 'After'
+
+        const hookStep: JsonStep = {
+          ariaSnapshot: null,
+          commands: [],
+          keyword: type,
+          data: {},
+          networkData: [],
+          result: {
+            status: 'FAILED',
+            message: testStepResult.message,
+            startTime: this.getTimeStamp(timestamp),
+            endTime: this.getTimeStamp(timestamp),
+          },
+          text: 'Failed hook',
+          type,
+          webLog: [],
+        }
+
+        if (type === 'Before') {
+          testCase.steps = [hookStep, ...testCase.steps]
+        } else {
+          testCase.steps = [...testCase.steps, hookStep]
+        }
+
+        this.testCaseReportMap.set(testStepFinished.testCaseStartedId, testCase)
         console.error(
           `Before/After hook failed with message: ${testStepResult.message}`
         )
